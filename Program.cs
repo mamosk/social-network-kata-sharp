5d4a444eacee157d59edc19053120feb67172e98
Fabio Michelini <fabio.michelini@euei.it>
2020-07-21 11:57:50 +0200

    help

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using social_network_kata_sharp.io;
 
 namespace social_network_kata_sharp
 {
@@ -10,7 +12,7 @@ namespace social_network_kata_sharp
             switch (args.Length)
             {
                 case 0:
-                    // TODO kata
+                    kata();
                     return 0;
                 case 1:
                     switch (args[0])
@@ -28,5 +30,95 @@ namespace social_network_kata_sharp
                     return 1;
             }
         }
+
+        private static void kata()
+        {
+            Console.SetOut(new PrefixedTextWriter());
+            string[] cmd;
+            string user;
+            while (true)
+            {
+                Console.Write("");
+                cmd = Console.ReadLine().Split(" ");
+                switch (cmd.Length)
+                {
+                    // No command
+                    case 0: continue;
+                    // Single word command
+                    case 1:
+                        switch (cmd[0])
+                        {
+                            // Exit
+                            case "exit":
+                                return;
+
+                            // Help
+                            case "help":
+                                break;
+
+                            // Read
+                            default:
+                                // TODO read user timeline
+                                user = cmd[0];
+                                continue;
+                        }
+                        break;
+
+                    // Multiple word command
+                    default:
+                        user = cmd[0];
+                        switch (cmd[1])
+                        {
+                            // Post
+                            case "->":
+                                // TODO post to user timeline
+                                continue;
+
+                            // Follow
+                            case "follows":
+                                // TODO user follows another user
+                                continue;
+
+                            // Wall
+                            case "wall":
+                                // TODO read user and zir followees timelines
+                                continue;
+
+                            default:
+                                break;
+                        }
+                        break;
+                }
+                help();
+            }
+        }
+
+        private const string HELP = @"
+kata commands:
+  <user name> -> <message>           -> post message to user timeline
+  <user name>                        -> read messages from user timeline
+  <user name> follows <another user> -> subscribe user to another user timeline
+  <user name> wall                   -> read messages from user timeline and subscriptions
+
+utility commands:
+  exit -> exit the cli
+  help -> read this help
+
+kata readme: https://github.com/xpeppers/social_networking_kata
+            ";
+        private static void help()
+        {
+            using (StringReader reader = new StringReader(HELP))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
     }
+
+
 }
